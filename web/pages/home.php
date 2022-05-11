b574282694560d82c5ef328c67aa49ad65d138a2
AtnRic <antoine.richard@student.junia.com>
2022-05-11 22:36:55 +0200

    page d'accueil

--- web/pages/home.php
+++ web/pages/home.php
@@ -1,1 +1,40 @@
+<!DOCTYPE html>
+<html lang='fr'>
+
+<!--le header se trouve dans le fichier header.php-->
 <?php
+$pageName="Home"; //le nom de la page
+$cssPath = "home.css";
+
+require '../tools/header.php';
+require '../tools/_main_tools.php';
+?>
+<body>
+<h1>Welcome to the Super Labyrinth 3000 deluxe !</h1>
+<h2>Choose your level</h2>
+<div class="container">
+    <div class="easy">
+        <div class="tuile"></div>
+        <h3>Welcome to the jungle</h3>
+        <p>Easy</p>
+    </div>
+    <div class="medium">
+        <div class="tuile"></div>
+        <h3>????</h3>
+        <p>Medium</p>
+    </div>
+    <div class="hard">
+        <div class="tuile"></div>
+        <h3>A space journey</h3>
+        <p>Hard</p>
+    </div>
+</div>
+<h2>Or create your own !</h2>
+<div class="container">
+    <div class="create">
+        <div class="tuile"></div>
+        <h3>Your own template</h3>
+    </div>
+</div>
+
+</body>
--- web/tools/header.php
+++ web/tools/header.php
@@ -1,1 +1,20 @@
 <?php
+if(!isset($pageName)){
+    $pageName ="";//si le nom de la page n'a pas été precise, on initialise la variable pour eviter une erreur
+}
+if(!isset($cssPath)){
+    $cssPath="";
+}
+
+echo "
+<head>
+    <meta charset='utf-8'/>
+    <meta name='description' content='Super Labyrinth 3000 deluxe'>
+    <meta name='keywords' content='Super Labyrinth 3000 deluxe'>
+    <meta name='author' content='Antoine RICHARD, Adrien VERHAEGHE, Enzo SERGIANI, Hippolyte DEPARIS, Matthieu BAUM, Thibaut LEMICHEL'>
+    <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+    <title>$pageName - S.L 3000</title>
+    <script src='https://kit.fontawesome.com/235593db07.js' crossorigin='anonymous'></script>
+    <link rel='stylesheet' href='../css/$cssPath'>
+    <link rel='icon' type='image/png' href='../images/bat.png'/>
+</head>";
