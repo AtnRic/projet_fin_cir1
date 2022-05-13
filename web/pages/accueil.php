f2acc7da4378e0ff11c0880cb4ef2ab1c38b163b
Enzo SERGIANI <enzo.sergiani@student.junia.com>
2022-05-13 15:24:47 +0200

    Correction page acceuil

--- web/pages/accueil.php
+++ web/pages/accueil.php
@@ -1,13 +1,14 @@
 <?php
-    session_start();
+session_start();
 ?>
 <html>
 <style>
-<?php
-    
+    <?php
+
     include "../css/accueil.css";
-?>
+    ?>
 </style>
+
 <body>
     <div id="text">
         <h1>THIS IS WHERE THE ADVENTURE BEGIN</h1>
@@ -27,57 +28,57 @@
     <!--Pop-up d'inscription-->
     <div id="inscription" class="overlay">
         <div class="popup">
-            <a class="close" href=""><img class ='_close' src="../images/x-mark-xxl.png" alt="X"></a>
-            <img class ='logo' src="images/bat.png" alt="logo">
+            <a class="close" href=""><img class='_close' src="../images/accueil/x-mark-xxl.png" alt="X"></a>
+            <img class='logo' src="images/bat.png" alt="logo">
             <h1>Create an account</h1>
             <form action="" method='POST'>
-                <label for="pseudo"></label><input type="text" id="pseudo" name="pseudo" placeholder="Pseudo" autofocus required ><br>
+                <label for="pseudo"></label><input type="text" id="pseudo" name="pseudo" placeholder="Pseudo" autofocus required><br>
                 <label for="mdpin"></label><input type="password" id="mdpin" name="mdpin" placeholder="Password (8 Charactere, maj, min et chiffres)" required><br>
                 <label for="verifmdp"></label><input type="password" id="verifmdp" name="verifmdp" placeholder="Confirm Password" required><br><br>
                 <input type="submit" class='sub' name='sub' value="Send">
-            </form> 
+            </form>
         </div>
     </div>
 
     <!--Popup de connexion-->
     <div id="connexion" class="overlay">
         <div class="popup">
-            <a class="close" href=""><img class ='_close' src="../images/x-mark-xxl.png" alt="X"></a>
-            <img class ='logo' src="images/bat.png" alt="logo">
+            <a class="close" href=""><img class='_close' src="../images/accueil/x-mark-xxl.png" alt="X"></a>
+            <img class='logo' src="images/bat.png" alt="logo">
             <h1>Login</h1>
             <form action="" method='POST'>
                 <label for="login"></label><input type="text" id="login" name="login" placeholder="Pseudo" autofocus required><br>
                 <label for="mdpco"></label><input type="password" id="mdpco" name="mdpco" placeholder="Password" required><br>
-            <input type="submit" class='sub' value="Send">
-            </form> 
-            <a class="mdp" href="pages/mdp.html">Lapse of Memory ?</a>  
+                <input type="submit" class='sub' value="Send">
+            </form>
+            <a class="mdp" href="pages/mdp.html">Lapse of Memory ?</a>
         </div>
     </div>
 
     <!--popup d'information pour les anonymes-->
     <div id="anonymes" class="overlay">
         <div class="popup">
-            <a class="close" href=""><img class ='_close' src="../images/x-mark-xxl.png" alt="X"></a>
-            <img class ='logo' src="images/bat.png" alt="logo">
+            <a class="close" href=""><img class='_close' src="../images/accueil/x-mark-xxl.png" alt="X"></a>
+            <img class='logo' src="images/bat.png" alt="logo">
             <h1>Informations importantes</h1>
             <p>en acceptant de jouer en temps qu'invité vous n'aurez pas accès au tableau des score et votre score ne sera pas inscrit dans ce dernier</p>
             <form action="" method='POST'>
                 <label for="login"></label><input type="text" id="login" name="anonymes" placeholder="Pseudo" autofocus required><br>
-            <input type="submit" name="subanonymes" class='sub' value="Send">
-            </form> 
+                <input type="submit" name="subanonymes" class='sub' value="Send">
+            </form>
         </div>
     </div>
 </body>
+
 </html>
 <?php
-    include '_main_tools.php';
-    // Register.
-        $count = 0;
-        if(isset($_POST['mdpin']))
-        {
-            if(!passwd($_POST['mdpin'])){
-                echo Console("wrong Password.");
-                echo "<style>
+include '_main_tools.php';
+// Register.
+$count = 0;
+if (isset($_POST['mdpin'])) {
+    if (!passwd($_POST['mdpin'])) {
+        echo Console("wrong Password.");
+        echo "<style>
                 .popup #mdpin{
                     outline: none;
                     border-style: solid;
@@ -85,15 +86,14 @@
                     border-width: 2px;
                     border-color:red;
                 }
-                </style>"; 
-            }
-            else{
-                $count++;
-            }
-            //on profite du isset pour le mot de passe pour verifier que les autres champs soient biens remplis
-            if(empty($_POST['pseudo'])){
-                echo Console("Pseudo empty.");
-                echo "<style>
+                </style>";
+    } else {
+        $count++;
+    }
+    //on profite du isset pour le mot de passe pour verifier que les autres champs soient biens remplis
+    if (empty($_POST['pseudo'])) {
+        echo Console("Pseudo empty.");
+        echo "<style>
                     .popup #pseudo{
                         outline: none;
                         border-style: solid;
@@ -101,16 +101,15 @@
                         border-width: 2px;
                         border-color:red;
                     }
-                    </style>"; 
-            }
-            else{
-                $count++;
-            }
+                    </style>";
+    } else {
+        $count++;
+    }
 
-        if(empty($_POST['mdpin'])){
-            echo Console("Password empty.");
+    if (empty($_POST['mdpin'])) {
+        echo Console("Password empty.");
 
-                echo "<style>
+        echo "<style>
                     .popup #mdpin{
                         outline: none;
                         border-style: solid;
@@ -118,12 +117,12 @@
                         border-width: 2px;
                         border-color:red;
                     }
-                    </style>"; 
-            }
-            if(empty($_POST['verifmdp'])){
-                echo Console("Password empty.");
+                    </style>";
+    }
+    if (empty($_POST['verifmdp'])) {
+        echo Console("Password empty.");
 
-                echo "<style>
+        echo "<style>
                     .popup #verifmdp{
                         outline: none;
                         border-style: solid;
@@ -131,11 +130,10 @@
                         border-width: 2px;
                         border-color:red;
                     }
-                    </style>"; 
-            }
-        }
-        else{
-            echo "<style>
+                    </style>";
+    }
+} else {
+    echo "<style>
             .popup input[type=text]:focus, input[type=password]:focus{
                 outline: none;
                 border-style: solid;
@@ -144,14 +142,14 @@
                 border-color:white;
             }
             </style>";
-        }
+}
 
-        //on vérifie les mdp (inscription)
-        if(isset($_POST['mdpin']) && isset($_POST['verifmdp'])){
-            if(!verifpasswd($_POST['mdpin'], $_POST['verifmdp'])){
-                echo Console("Password 1 != 2");
+//on vérifie les mdp (inscription)
+if (isset($_POST['mdpin']) && isset($_POST['verifmdp'])) {
+    if (!verifpasswd($_POST['mdpin'], $_POST['verifmdp'])) {
+        echo Console("Password 1 != 2");
 
-                echo "<style>
+        echo "<style>
                 .popup #verifmdp, #mdpin{
                     outline: none;
                     border-style: solid;
@@ -159,24 +157,21 @@
                     border-width: 2px;
                     border-color:red;
                 }
-                </style>"; 
-            }
-            else{
-                $count++;
-            }
-        }
-        
-        if($count == 3)
-        {
-            $sign = signup($_POST['pseudo']);
+                </style>";
+    } else {
+        $count++;
+    }
+}
 
-            if($sign){
-            echo Console("Connection granted.");
-            $count++;
-            }  
-            else{            
-            echo Console("Signup fail.");
-            echo "<style>
+if ($count == 3) {
+    $sign = signup($_POST['pseudo']);
+
+    if ($sign) {
+        echo Console("Connection granted.");
+        $count++;
+    } else {
+        echo Console("Signup fail.");
+        echo "<style>
             .popup #mdpin{
                 outline: none;
                 border-style: solid;
@@ -184,34 +179,33 @@
                 border-width: 2px;
                 border-color:red;
             }
-            </style>"; 
-            }
-        }
+            </style>";
+    }
+}
 
-        //si les 3 conditions sont vérifiées :
-        if($count==4)
-        {
-            echo Console("Not to twooter.");
+//si les 3 conditions sont vérifiées :
+if ($count == 4) {
+    echo Console("Not to twooter.");
 
-            $login= $_POST['pseudo'];
-            $mdp_hash = hash('sha256', $_POST['mdpin']);//on fait un hash du mot de passe pour ne pas stocker le mot de passe en clair
-            $_SESSION["pseudo"]=$login; //Variable de session "pseudo"
-            if (!isset($_COOKIE['pseudo']) && !isset($_COOKIE['mdp_hash'])){
-                setcookie("login", $login, time() + (3600 * 24 * 365));
-                setcookie("mdp_hash", $mdp_hash, time() + (3600 * 24 * 365));
-            }
-            $newURL = "home.php";
-            header('Location: '.$newURL);
-            die();
-        }
+    $login = $_POST['pseudo'];
+    $mdp_hash = hash('sha256', $_POST['mdpin']); //on fait un hash du mot de passe pour ne pas stocker le mot de passe en clair
+    $_SESSION["pseudo"] = $login; //Variable de session "pseudo"
+    if (!isset($_COOKIE['pseudo']) && !isset($_COOKIE['mdp_hash'])) {
+        setcookie("login", $login, time() + (3600 * 24 * 365));
+        setcookie("mdp_hash", $mdp_hash, time() + (3600 * 24 * 365));
+    }
+    $newURL = "home.php";
+    header('Location: ' . $newURL);
+    die();
+}
 
-    // Login.
-        $count = 0;
-        if(isset($_POST['mdpco'])){
+// Login.
+$count = 0;
+if (isset($_POST['mdpco'])) {
 
-                //on profite du isset pour le mot de passe pour verifier que les autres champs soient biens remplis
-                if(empty($_POST['login'])){
-                    echo "<style>
+    //on profite du isset pour le mot de passe pour verifier que les autres champs soient biens remplis
+    if (empty($_POST['login'])) {
+        echo "<style>
                         .popup #login{
                             outline: none;
                             border-style: solid;
@@ -219,21 +213,18 @@
                             border-width: 2px;
                             border-color:red;
                         }
-                        </style>"; 
-                }
-                else{
-                    $count++;
-                }
+                        </style>";
+    } else {
+        $count++;
+    }
 
-                if($count == 1)
-                {                        
-                    $sign = signin($_POST['login']);
-                    if($sign){
-                        $count++;
-                        echo Console("Connection granted.");
-                    }   
-                    else{
-                    echo "<style>
+    if ($count == 1) {
+        $sign = signin($_POST['login']);
+        if ($sign) {
+            $count++;
+            echo Console("Connection granted.");
+        } else {
+            echo "<style>
                     .popup #login{
                         outline: none;
                         border-style: solid;
@@ -241,12 +232,11 @@
                         border-width: 2px;
                         border-color:red;
                     }
-                    </style>"; 
-                    }
-                }
+                    </style>";
         }
-        else{
-                echo "<style>
+    }
+} else {
+    echo "<style>
                 .popup input[type=text]:focus, input[type=password]:focus{
                     outline: none;
                     border-style: solid;
@@ -255,30 +245,29 @@
                     border-color:white;
                 }
                 </style>";
-        }
-            
-        //si les 2 conditions sont vérifiées :
-        if($count==2)
-        {
-                $login= $_POST['login'];
-                $mdp_hash = hash('sha256', $_POST['mdpco']);//on fait un hash du mot de passe pour ne pas stocker le mot de passe en clair
-                $_SESSION["pseudo"]=$login; //Variable de session "pseudo"
-                if (!isset($_COOKIE['pseudo']) && !isset($_COOKIE['mdp_hash'])){
-                    setcookie("login", $login, time() + (3600 * 24 * 365));
-                    setcookie("mdp_hash", $mdp_hash, time() + (3600 * 24 * 365));
-                }
-                //ajouter utilisateur dans la base de données
-                $newURL = "home.php";
-                header('Location: '.$newURL);
-                die();
-        }
-    
-    //anonymes
-        if(isset($_POST["subanonymes"])){
-            if(empty($_POST['anonymes'])){
-            echo Console("Pseudo anonyme empty.");
+}
 
-                echo "<style>
+//si les 2 conditions sont vérifiées :
+if ($count == 2) {
+    $login = $_POST['login'];
+    $mdp_hash = hash('sha256', $_POST['mdpco']); //on fait un hash du mot de passe pour ne pas stocker le mot de passe en clair
+    $_SESSION["pseudo"] = $login; //Variable de session "pseudo"
+    if (!isset($_COOKIE['pseudo']) && !isset($_COOKIE['mdp_hash'])) {
+        setcookie("login", $login, time() + (3600 * 24 * 365));
+        setcookie("mdp_hash", $mdp_hash, time() + (3600 * 24 * 365));
+    }
+    //ajouter utilisateur dans la base de données
+    $newURL = "home.php";
+    header('Location: ' . $newURL);
+    die();
+}
+
+//anonymes
+if (isset($_POST["subanonymes"])) {
+    if (empty($_POST['anonymes'])) {
+        echo Console("Pseudo anonyme empty.");
+
+        echo "<style>
                     .popup #login{
                         outline: none;
                         border-style: solid;
@@ -286,12 +275,11 @@
                         border-width: 2px;
                         border-color:red;
                     }
-                    </style>"; 
-            }
-            else{
-            $_SESSION["pseudo"]=$_POST['anonymes'];
-            header("location:home.php");
-            }
-        }
-    
-    ?>
+                    </style>";
+    } else {
+        $_SESSION["pseudo"] = $_POST['anonymes'];
+        header("location:home.php");
+    }
+}
+
+?>
