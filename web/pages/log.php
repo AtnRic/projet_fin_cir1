3d21d9afe380fec9d5341ebb5b314ef6e567529a
AtnRic <antoine.richard@student.junia.com>
2022-05-17 21:30:47 +0200

    test page connexion

---
+++ web/pages/log.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>retro</title>
+    <link rel="stylesheet" type="text/css" href="../css/log.css">
+</head>
+<body>
+<div class="press">
+    <h1 id="name">Tartaros fantasy</h1>
+    <a href="#choose">
+        <h1 id="press">PRESS START</h1>
+    </a>
+
+</div>
+
+<div class="choose" id="choose">
+    <h1>Choose your player :</h1>
+    <div class="container players">
+        <div class="signin but">
+            <div class="tuile"></div>
+            <h3>Sign In</h3>
+        </div>
+        <div class="signup but">
+            <div class="tuile"></div>
+            <h3>Sign Up</h3>
+        </div>
+        <div class="without but">
+            <div class="tuile"></div>
+            <h3>Play without account</h3>
+        </div>
+    </div>
+</div>
+</body>
