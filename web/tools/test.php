921c348dc2977218796acf9f5f5a3011b7f47562
AtnRic <antoine.richard@student.junia.com>
2022-05-17 11:26:36 +0200

    test call c function

--- web/tools/test.php
+++ web/tools/test.php
@@ -1,6 +1,20 @@
 <?php
-exec("../.../c/Solution1/main2.exe", $output, $result_code);
+//$cli = `../.../c/Solution1/main3.exe 1 a`;
+//exec('../.../c/Solution1/main3.exe', $output, $result_code);
+//$out=" ";
+//exec("../.../c/Solution1/main4.exe 125 70", $out);
+//print_r($out);
 
-var_dump($output);
+//var_dump($output);
 
-var_dump($result_code);
+//var_dump($result_code);
+
+
+
+// create FFI object, loading libc and exporting function printf()
+$ffi = FFI::cdef(
+    "int printf(const char *format, ...);", // this is a regular C declaration
+    "libc.so.6");
+// call C's printf()
+$ffi->printf("Hello %s!\n", "world");
+
