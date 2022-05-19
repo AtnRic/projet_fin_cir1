c8f9beb994a248f4e93f9b41aed92cd8105f8c43
Maskrpone <deparis.hipp@gmail.com>
2022-05-19 10:43:20 +0200

    home page

--- web/tools/header.php
+++ web/tools/header.php
@@ -13,8 +13,11 @@ echo "
     <meta name='keywords' content='Super Labyrinth 3000 deluxe'>
     <meta name='author' content='Antoine RICHARD, Adrien VERHAEGHE, Enzo SERGIANI, Hippolyte DEPARIS, Matthieu BAUM, Thibaut LEMICHEL'>
     <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
     <title>$pageName - S.L 3000</title>
     <script src='https://kit.fontawesome.com/235593db07.js' crossorigin='anonymous'></script>
-    <link rel='stylesheet' href='../css/$cssPath'>
     <link rel='icon' type='image/png' href='../images/bat.png'/>
-</head>";
+    <link rel='stylesheet' href='../css/home.css'>
+    </head>";
+
+// <link rel='stylesheet' href='../css/home.css'>
