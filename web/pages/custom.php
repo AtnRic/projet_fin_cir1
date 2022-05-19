0ebcc20d9b47b80903cf07370e757b1921838473
Enzo SERGIANI <enzo.sergiani@student.junia.com>
2022-05-19 15:08:36 +0200

    Update custom

--- web/pages/custom.php
+++ web/pages/custom.php
@@ -4,27 +4,30 @@
 <link rel="stylesheet" type="text/css" href="../css/custom.css">
 </head>
 <body>
-
-<div class="main">
-  <label for="panel_size">Taille du labyrinthe</label>
+  <div class="title">
+    <h1>Welcome to the sandbox</h1>
+    <h4>Here you can create your own maze</h4>
+  </div>
+<div class="range">
+  <!--[X] : valeur qui changera selon la position du curseur-->
+  <label for="panel_size">Taille du labyrinthe : [X]</label>
   <input
       type="range"
       name="participants"
       min="5"
       max="51"
       value="20"
-  >
-  <span class="rangeslider__tooltip" id ="range-tooltip"></span>  
-  <label for="panel_size">Nombre de garde</label>
-  <input
+    >
+    <br>
+    <label for="panel_size">Nombre de garde : [X]</label>
+    <input
       type="range"
       name="participants"
       min="5"
-      
-      max="40"
-      value="5"
-  >
-  <span class="rangeslider__tooltip" id ="range-tooltip"></span>
+      max="51"
+      value="20"
+    > 
+  <button>Générer</button>
 </div>
 
 </body>
