7312a86fc5142f2d52800d5304815a6b4efdef91
AdrVrh <adrienverhaeghe@gmail.com>
2022-05-19 10:46:13 +0200

    custom

---
+++ web/pages/custom.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang='fr'>
+<head>
+<link rel="stylesheet" type="text/css" href="../css/custom.css">
+</head>
+<body>
+
+<div class="main">
+  <label for="panel_size">Taille du labyrinthe</label>
+  <input
+      type="range"
+      name="participants"
+      min="5"
+      max="51"
+      value="20"
+  >
+  <span class="rangeslider__tooltip" id ="range-tooltip"></span>  
+  <label for="panel_size">Nombre de garde</label>
+  <input
+      type="range"
+      name="participants"
+      min="5"
+      
+      max="40"
+      value="5"
+  >
+  <span class="rangeslider__tooltip" id ="range-tooltip"></span>
+</div>
+
+</body>
