bd43d399a8116a6dec454bc518b48ffc40627d63
Thibaut LEMICHEL <thibaut.lemichel@student.junia.com>
2022-05-23 11:36:05 +0200

    azert

--- c/Solutions2/Source.c
+++ c/Solutions2/Source.c
@@ -1,72 +1,60 @@
-#include <stdlib.h>
 #include <stdio.h>
-#include <conio.h>
-#include <string.h>
+#include <stdlib.h>
 
-#define MAX 10
-
-double* extract(char* nomFichier)
+void main(void)
 {
-    char tampon[MAX] = { 0 };
-    int i, n = 0, j, h;
-    FILE* fichier = NULL;
-    double* tableau;
-
-    printf("Le fichier a ouvrir est : %s\n", nomFichier);
-    fichier = fopen(nomFichier, "r");
-
-    if (fichier != NULL)
+    FILE* fic;
+    char ligne[81];
+    char* ptr_chaine; // pointeur pour balayer les sous-chaînes obtenues
+    short int num_ligne = 1;
+    short int data_entier;
+    double data_reel;
+    char data_chaine[11];
+    //----------------------- ouverture du fichier de données CSV ------------------------------
+    fic = fopen_s("fic_data.csv", "rt");
+    errno_t erCode = fopen_s(&fic, "fic_data.csv", "rt");
+    if (fic == NULL)
     {
-        printf("L'ouverture du fichier %s a reussie !\n", nomFichier);
-
-        while (fgets(tampon, MAX, fichier) != NULL)
-            n++;
-
-        printf("Le fichier contient %d valeurs.\n", n);
-
-        tableau = malloc(n * sizeof(double));
-
-        if (tableau == NULL)
-        {
-            printf("Erreur allocation !");
-            exit(0);
-        }
-        else
-        {
-            printf("Allocation reussie !\n");
-        }
-
-        rewind(fichier);
-
-        for (h = 0; h < MAX; h++)
-            tableau[h] = 0.0;
-
-        for (i = 0; i < n; i++)
-        {
-            fgets(tampon, MAX, fichier);
-            printf("tampon : %s\n", tampon);
-
-            tableau[i] = atof(tampon);
-        }
-
-        for (j = 0; j < n; j++)
-            printf("%f\n", tableau[j]);
+        printf("Ouverture fichier impossible !");
+        exit(0);
     }
-    else
-        printf("L'ouverture du fichier %s a echouee !\n", nomFichier);
-
-    return tableau;
-}
-
-int main(void)
-{
-    int     i;
-    char nf[255] = "fichier.txt";
-    double* nbre;
-    nbre = extract(nf);
-
-    for (i = 0; i < MAX; i++)
-        printf("--> %f\n", nbre[i]);
-
-    return 0;
+    //----------------------- lecture du fichier de données CSV ------------------------------
+    // on lit une ligne après l'autre jusqu'�  la fin du fichier
+    while (fgets(ligne, 81, fic) != NULL)
+    {
+        printf("\n Ligne %2hd :", num_ligne);
+        num_ligne++;
+        ptr_chaine = strtok(ligne, ";"); // appel d'initialisation de strtok. Séparateur = ';'
+        /* on lit une cellule (colonne) après l'autre jusqu'�  la fin de la ligne. Notez que si les cellules contenaient
+        des données de même type, on pourrait écrire une boucle while(ptr_chaine!=NULL){} */
+        // cellule 1 :
+        if (sscanf_s(ptr_chaine, "%s", data_chaine) != 1) // verif. de la validité des données
+        {
+            puts("\nPb de lecture cellule 1 (chaine) !");
+            data_chaine[0] = 0;
+        }
+        ptr_chaine = strtok(NULL, ";"); // remplace le prochain séparateur trouvé par 0,
+// puis renvoie l'adresse de la chaîne ainsi obtenue. Séparateur = ';'
+// cellule 2 :
+        if (sscanf_s(ptr_chaine, "%hd", &data_entier) != 1)
+        {
+            puts("\nPb de lecture cellule 2 (entier)!");
+            data_entier = -11111;
+        }
+        ptr_chaine = strtok(NULL, ";"); // remplace le prochain séparateur trouvé par 0
+// cellule 3 :
+        ptr_chaine = strtok(NULL, ";"); // on saute la cellule 3 supposée sans intérêt
+// cellule 4 :
+        if (sscanf_s(ptr_chaine, "%lf", &data_reel) != 1)
+        {
+            puts("\nPb de lecture cellule 4 (reel)!");
+            data_reel = -111.111;
+        }
+        ptr_chaine = strtok(NULL, ";"); // non indispensable ici
+// affichages :
+        printf("\n\t cellule 1 (chaine): %11s ", data_chaine);
+        printf("\n\t cellule 2 (entier): %11hd", data_entier);
+        printf("\n\t cellule 4 (reel) : %11.3lf", data_reel);
+    }
+    fclose(fic);
 }
