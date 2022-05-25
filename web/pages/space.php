9be8eb95fb554b291f1359408437eeff19306973
AtnRic <antoine.richard@student.junia.com>
2022-05-25 20:36:09 +0200

    ajout thème espace

---
+++ web/pages/space.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Space</title>
+    <link rel="stylesheet" type="text/css" href="../css/space.css">
+</head>
+
+<body>
+
+<div class="global">
+    <table id="container"></table>
+</div>
+</body>
+<script src="https://code.jquery.com/jquery-3.1.1.min.js"   integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8="   crossorigin="anonymous"></script>
+<script src="../js/node_modules/animejs/lib/anime.min.js"></script>
+<script src="../js/main.js">
+</script>
+<script>
+    SetAmbiance("Space");
+    PHP_Start(true);
+</script>
+
+</html>
