b5da7c9038d7908a312d891607c392b7f184ccad
ThibautLemichel <90193980+ThibautLemichel@users.noreply.github.com>
2022-06-06 22:39:16 +0200

    ME

--- c/Solution1/Test/test.cpp
+++ c/Solution1/Test/test.cpp
@@ -5,3 +5,8 @@ TEST(TestCaseName, TestName) {
   EXPECT_EQ(1, 1);
   EXPECT_TRUE(true);
 }
+
+TEST(ApparitionGardes, Test_Appartion_Gardes_basic) {
+	ApparitionGardes("eafbdaeertrezxdtytreytae", 5, 1, 0, recherche_loc_tp(0, "eafbdaeertrezxdtytreytae", 25));
+	EXPECT_TRUE(true);
+}
