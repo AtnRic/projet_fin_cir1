434db4f0aefc76c3452dd1a9dbeb07ad24d6bd58
ThibautLemichel <90193980+ThibautLemichel@users.noreply.github.com>
2022-06-07 13:54:39 +0200

    KA

--- c/Solution1/Test/test.cpp
+++ c/Solution1/Test/test.cpp
@@ -6,12 +6,28 @@ TEST(TestCaseName, TestName) {
   EXPECT_TRUE(true);
 }
 
-//TEST(AppartitionGardes, ApparitionGardes1) {
-//	ASSERT_STREQ("eafbdaeertrezxdtytreytae", "eafbdaeertrezxdtytreytae");
-//}
-//
-//TEST(ApparitionGardes, Test_Appartion_Gardes_basic) {
-//	ApparitionGardes("eafbdaeertrezxdtytreytae", 5, 1, 0, recherche_loc_tp(0, "eafbdaeertrezxdtytreytae", 25));
-//	EXPECT_TRUE(true);
-//}
+TEST(recherche_loc_tp, recherche_loc_tp1) {
+	Teleporteurs_Paire* test_return;
+	test_return = (Teleporteurs_Paire*)malloc(sizeof(Teleporteurs_Paire));
+	int nb_pairs = 1;
+	char* maze = "aaaaaaaaaaaaaaaaaaaaaaaa";
+	int size = 25;
+	test_return = recherche_loc_tp(nb_pairs, maze, size);
+	EXPECT_TRUE(test_return == NULL)
+		<< "Where test_return value: " << *test_return
+		<< " not equal : " << NULL
+		<< ".";
+}
+
+TEST(recherche_loc_tp, recherche_loc_tp2) {
+	Teleporteurs_Paire* test_return;
+	test_return = (Teleporteurs_Paire*)malloc(sizeof(Teleporteurs_Paire));
+	int nb_pairs = 1;
+	char* maze = "lmaaaaaaaaaaaaaaaaaaaaaa";
+	int size = 25;
+	test_return = recherche_loc_tp(nb_pairs, maze, size);
+	EXPECT_TRUE((test_return->entree == 1 && test_return->sortie == 2) || (test_return->sortie == 1 && test_return->entree == 2))
+		<< "Where test_return entree value: " << test_return->entree << " and test_return sortie value: " << test_return->sortie
+		<< "are not equal neither 1 and 2" << " or 2 and 1.";
+}
 
