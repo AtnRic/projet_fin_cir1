71ddd7b27b6bc89f409267400f322a2c54ead39c
MatthieuBaum <90194395+MatthieuBaum@users.noreply.github.com>
2022-06-09 10:46:39 +0200

    bfeqfq

--- web/pages/points.php
+++ web/pages/points.php
@@ -5,6 +5,7 @@
     if(isset($_POST['functionParameters'])){
         $data = $_POST['functionParameters'];
     }
+    //si la fonction finish est appeler en js (utilisateur finie arrive sur la case de fin) on rajoute en php (taille du labyrinthe x 10) points a l'utilisateur connectée
     if($functionName == 'finish'){
         $connexion=connect();
         $username=$_COOKIE["login"];
@@ -24,9 +25,11 @@
         }*/
         $resultat=mysqli_query($connexion,"UPDATE users SET Nbr_Points='$points' WHERE Pseudo='$username'");
     }
+    //si l'utilisateur effectue le chemin le plus court il reçoit 300 points en plus que les points de base
     if($functionName == 'short'){
         $connexion=connect();
         $username=$_COOKIE["login"];
+        //récupére dans la base les points de l'utilisateur
         $points=GetUserPoints($_COOKIE["login"]);
         $points=$points+($data*10)+300;
         echo"\npoints de départ user :\n";
@@ -36,9 +39,11 @@
         echo"\nSHORT !!!\n";
         $resultat=mysqli_query($connexion,"UPDATE users SET Nbr_Points='$points' WHERE Pseudo='$username'");
     }
+    //si l'utilisateur effectue le chemin le plus long il reçoit 300 points en plus que les points de base
     if($functionName == 'long'){
         $connexion=connect();
         $username=$_COOKIE["login"];
+        //récupére dans la base les points de l'utilisateur
         $points=GetUserPoints($_COOKIE["login"]);
         $points=$points+($data*10)+300;
         echo"\npoints de départ user :\n";
